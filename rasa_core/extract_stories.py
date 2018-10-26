53d6ddb4dbf2f119cc8040509382d90dbf31dbe3
Alan Nichol <alan@golastmile.com>
2018-10-26 11:41:43 +0200

    update extract stories script https://github.com/RasaHQ/rasa_core/issues/1215

--- rasa_core/extract_stories.py
+++ rasa_core/extract_stories.py
@@ -3,12 +3,82 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from rasa_core.utils import EndpointConfig
-import io
 from tqdm import tqdm
+import argparse
+import logging
+import io
 
-CORE_HOST = "http://localhost:5005"
-core_endpoint = EndpointConfig(CORE_HOST, token='coretoken')
+from rasa_core import utils
+
+logger = logging.getLogger(__name__)
+
+# constants
+UNKNOWN = "UNKNOWN"
+SUCCESSFUL = "SUCCESSFUL"
+UNSUCCESSFUL = "UNSUCCESSFUL"
+MEMORIZED = "MEMORIZED"
+PREDICTED = "PREDICTED"
+
+template = {
+    SUCCESSFUL: {
+        MEMORIZED: [],
+        PREDICTED: [],
+    },
+    UNSUCCESSFUL: {
+        MEMORIZED: [],
+        PREDICTED: [],
+    },
+}
+
+def create_argument_parser():
+    """Parse all the command line arguments for the training script."""
+
+    parser = argparse.ArgumentParser(
+            description='fetches conversations from a rasa core server to use for training')
+    parser = add_args_to_parser(parser)
+
+    utils.add_logging_option_arguments(parser)
+    return parser
+
+def add_args_to_parser(parser):
+
+    def try_parse_date(date):
+        try:
+            return datetime.strptime(date, "%Y-%m-%d")
+        except ValueError:
+            message = "date had an invalid format '{}' cannot be parsed.".format(s)
+            raise argparse.ArgumentTypeError(message)
+
+    parser.add_argument(
+            '-o', '--out',
+            type=str,
+            required=False,
+            help="directory to persist the trained model in"
+    )
+    parse.add_argument(
+            '--host',
+            type=str,
+            required=True,
+            help="hostname where rasa core server is running"
+    )
+    parse.add_argument(
+            '-t', '--token',
+            type=str,
+            required=False,
+            help="rasa core server token"
+    )
+    parser.add_argument(
+            "-d", "--date", 
+            help="date in format YYYY-MM-DD , only fetch stories with a latest_message after this date",
+            required=False, 
+            type=try_parse_date)
+    parser.add_argument(
+            "-N", "--num_stories", 
+            help="maximum number of stories to evaluate, defaults to 100",
+            required=False,
+            default=100,
+            type=int)
+    return parser
 
 def fetch_sender_ids():
     result = core_endpoint.request(method="get", subpath="/conversations")
@@ -28,35 +98,71 @@ def fetch_stories(n=None):
         for _id in sender_ids]
 
 def evaluate(story):
+    memorized = MEMORIZED
     result = core_endpoint.request(
         subpath="/evaluate",
         content_type="text/markdown",
         data=story.encode('utf-8'))
-    return result.json()
 
-def is_successful(story):
-    raise NotImplementedError()
+    if result['in_training_data_fraction'] < 1:
+        memorized = PREDICTED
+    return result.json(), memorized
 
+def write_results(results, output_dir=None):
+
+    def fpath(goal, success, memo):
+        name = "{}_{}_{}.md".format(goal, success, memo)
+        if output_dir:
+            name = os.path.join(output_dir, name)
+        return name
+
+    for goal in results.keys():
+        for success in results[goal].keys():
+            for memo in results[goal][success].keys():
+                stories = results[goal][success][memo]
+                if not stories:
+                    continue
+
+                file_path = fpath(goal, success, memo)
+                with io.open(file_path, "w") as f:
+                    f.write("\n".join(stories))
+
+def goal_success(story):
+    user_goal, success = UNKNOWN, UNSUCCESSFUL
+
+    if "signup_newsletter" in story:
+        user_goal = "signup_newsletter"
+        if "action_subscribe_newsletter" in story:
+        success = SUCCESSFUL
+
+    return user_goal, success
+    
 
 if __name__ == "__main__":
 
-    correct_predict = io.open("correct_predict.md", "w")
-    incorrect_predict = io.open("incorrect_predict.md", "w")
-    incorrect_memo = io.open("incorrect_memo.md", "w")
-    N = 150
+    arg_parser = create_argument_parser()
+    cmdline_args = arg_parser.parse_args()
 
-    all_stories = fetch_stories(n=N)
-    for i, story in tqdm(enumerate(all_stories)):
-        result = evaluate(story)
-        if 'in_training_data_fraction' in result:
-            memo_frac = result['in_training_data_fraction']
-            if memo_frac < 1.:
-                if is_successful(story):
-                    correct_predict.write(story+"\n")
-                else:
-                    incorrect_predict.write(story+"\n")
-            else:
-                if not is_successful(story):
-                    incorrect_memo.write(story+"\n")
+    utils.configure_colored_logging(cmdline_args.loglevel)
 
-    
+    endpoint = utils.EndpointConfig(
+        cmdline_args.host,
+        token=cmdline_args.token)
+
+    goals_to_ignore = []
+    results = {}
+
+    all_stories = fetch_stories(endpoint, max_stories=cmdline_args.num_stories)
+ 
+    for story in tqdm(all_stories):
+
+        goal, success = goal_success(story)
+        if goal in goals_to_ignore:
+            continue
+        _, memorized = evaluate(story)
+        if goal not in results:
+            results[goal] = template.copy()
+
+        results[goal][successful][memorized].append(story)
+
+    write_results(results, output_dir=cmdline_args.out)
