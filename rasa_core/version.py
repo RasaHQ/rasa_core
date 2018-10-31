7276460a1d1c3404b98ccc881feb322b002edc91
ricwo <ric.wkr@gmail.com>
2018-10-31 19:45:17 +0100

    increase version to 0.12.0a5

--- rasa_core/version.py
+++ rasa_core/version.py
@@ -1,7 +1,6 @@
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
 from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
 
-
-__version__ = '0.12.0a4'
+__version__ = '0.12.0a5'
